4325768e29fc4bf3459e794de443063f67c77010
Win Verma <win.verma2021@vitstudent.ac.in>
2024-05-30 19:37:34 +0530

    Time: 1 ms (33.96%) | Memory: 41.8 MB (81.28%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int k = m;
+        for(int i = 0; i<n; i++)
+        {
+            nums1[k++] = nums2[i];
+        }
+         Arrays.sort(nums1);
+    }
+}
