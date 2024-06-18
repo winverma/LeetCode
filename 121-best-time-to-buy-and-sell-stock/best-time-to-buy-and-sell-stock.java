f37d0b0f2d200702628cfdd46cca453da88a4453
Win Verma <win.verma2021@vitstudent.ac.in>
2024-06-19 02:58:11 +0530

    Time: 327 ms (8.59%) | Memory: 61.4 MB (62.73%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int maxProfit(int[] prices) 
+    {
+        int profit = 0;
+        int buy = prices[0];
+        for(int i=1; i<prices.length; i++)
+        {
+            if(prices[i] < buy)
+            {
+                buy = prices[i];
+            }
+            else if(prices[i] - buy > profit)
+            {
+                profit = prices[i]-buy;
+            }
+        }
+        return profit;
+    }
+}
