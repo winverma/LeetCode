fe825c5029d68508cca01b14792906f0e425bb2f
Win Verma <win.verma2021@vitstudent.ac.in>
2024-07-09 03:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (51.21%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int trailingZeroes(int n) 
+    {
+        if(n<5)
+        {
+            return 0;
+        }
+
+        int count = 0;
+        for(int i = 5; n/i >= 1; i *= 5)
+        {
+            count += n/i;
+        }
+
+        return count;
+    }
+}
