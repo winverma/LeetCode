71eed42b674fb6af8562c167f7edd34098c7574a
Win Verma <win.verma2021@vitstudent.ac.in>
2024-07-20 15:55:29 +0530

    Time: 75 ms (29.01%) | Memory: 41.9 MB (60.73%) - LeetSync

--- 35-search-insert-position/search-insert-position.cs
+++ 35-search-insert-position/search-insert-position.cs
@@ -4,10 +4,10 @@ public class Solution
     {
         int low = 0;
         int high = nums.Length-1;
-
+        int mid;
         while (low <= high) 
         {
-            int mid = low + (high-low)/2;
+            mid = low + (high-low)/2;
             if (nums[mid] == target) return mid;
             if (nums[mid] > target) high = mid-1;
             else low = mid+1;
