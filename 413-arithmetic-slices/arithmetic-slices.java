d02d3cd970d256496e4b1d8f468e929afe3c06e7
Win Verma <win.verma2021@vitstudent.ac.in>
2024-07-21 12:06:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (72.41%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.java
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -2,26 +2,28 @@ public class Solution
 {
     public int numberOfArithmeticSlices(int[] nums) 
     {
-        int n = nums.length;
-        
-        if (n < 3) return 0;
-        
         int count = 0;
         int current = 0;
-        
-        for (int i = 2; i < n; i++) 
+        int len = nums.length;
+
+        if(len < 3)
         {
-            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) 
+            return 0;
+        }
+        
+        for(int i = 2; i < len; i++)
+        {
+            if(nums[i-1] - nums[i-2] == nums[i] - nums[i-1])
             {
                 current++;
                 count += current;
-            } 
-            else 
+            }
+            else
             {
                 current = 0;
             }
         }
-        
+
         return count;
     }
 }
