6f642613f00e2ae63439dc11f6d5d16daa56dffd
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-19 03:10:49 +0530

    Time: 56 ms (92.36%) | Memory: 41.5 MB (44.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cs
@@ -1,13 +1,58 @@
-public class Solution 
+using System;
+
+class Solution
 {
-    public int Search(int[] nums, int target) 
+    public int Search(int[] nums, int target)
     {
+        int pivot = 0;
         int len = nums.Length;
 
-        for(int i = 0; i<len; i++)
+        if (len == 1 && target == nums[0])
         {
-            if(nums[i]==target) return i;
+            return 0;
         }
+
+        if (len <= 1 && target != nums[0])
+        {
+            return -1;
+        }
+
+        for (int i = 1; i < len; i++)
+        {
+            if (nums[i] < nums[pivot])
+            {
+                pivot = i;
+            }
+        }
+
+        int low = pivot;
+        int high = len - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (target < nums[mid]) high = mid - 1;
+
+            else low = mid + 1;
+        }
+
+        int left = 0;
+        int right = pivot;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (target < nums[mid]) right = mid - 1;
+
+            else left = mid + 1;
+        }
+
         return -1;
     }
 }
