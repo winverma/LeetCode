58791ec8e5aada6faebe12562669c2af73eb7182
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-21 01:59:41 +0530

    Time: 2 ms (85.91%) | Memory: 55.1 MB (78.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -14,14 +14,13 @@ class Solution
             result[i] = lpc;
         }
         
+        // Right Procduct Calculation (RPC)
         int rpc = 1;
         for(int i = len-1; i>=0; i--)
         {
             result[i] *= rpc;
             rpc *= nums[i];
         }
-
-       
 
         return result;
     }
