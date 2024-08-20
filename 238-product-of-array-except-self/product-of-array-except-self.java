dcac205c4c2cde1400472a1af1e6547d409bf2b6
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-21 01:58:59 +0530

    Time: 2 ms (85.91%) | Memory: 55.2 MB (65.97%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public int[] productExceptSelf(int[] nums) 
+    {
+        int len = nums.length;
+        int[] result = new int[len];
+        int lpc = 1;
+
+        // Left Product Calculation (LPC)
+        result[0] = 1;
+        for (int i = 1; i < len; i++) 
+        {
+            lpc *= nums[i-1];
+            result[i] = lpc;
+        }
+        
+        int rpc = 1;
+        for(int i = len-1; i>=0; i--)
+        {
+            result[i] *= rpc;
+            rpc *= nums[i];
+        }
+
+       
+
+        return result;
+    }
+}
+
