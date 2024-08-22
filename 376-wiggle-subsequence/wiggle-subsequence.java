fa0c56a3c4f7fc0893fe0fc4aad768fac845ff29
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-23 03:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (74.55%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -0,0 +1,31 @@
+class Solution 
+{
+    public int wiggleMaxLength(int[] nums) 
+    {
+        int len = nums.length;
+
+        int diff = -1;
+        int count = 1;
+
+        for(int i = 0; i<len-1; i++)
+        {
+            if(nums[i] > nums[i+1])
+            {
+                if(diff == 1 || diff == -1)
+                {
+                    diff = 0;
+                    count += 1;
+                }
+            }
+            if(nums[i] < nums[i+1])
+            {
+                if(diff == 0 || diff == -1)
+                {
+                    diff = 1;
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+}
