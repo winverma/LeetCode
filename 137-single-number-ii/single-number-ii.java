e760f428d7e03d515960c43cdfd06a5c5ad99d37
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-27 23:19:53 +0530

    Time: 4 ms (60.25%) | Memory: 45.5 MB (38.18%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    public int singleNumber(int[] nums) 
+    {
+        int ans = 0;
+        // Iterate through each bit position (0 to 31)
+        for (int i = 31; i >= 0; i--) {
+            int sum = 0;
+            // Count how many numbers have the ith bit set
+            for (int num : nums) {
+                int n = (num >> i);
+                if ((n & 1) == 1) {
+                    sum++;
+                }
+            }
+            // If sum is not a multiple of 3, the unique number has this bit set
+            sum = sum % 3;
+            // Shift ans left before adding the bit
+            ans = (ans << 1);
+            if (sum != 0) {
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+}
