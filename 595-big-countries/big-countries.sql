b66ebd09340d3e597563fe5adefb2e81b808154f
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-29 00:02:43 +0530

    Time: 333 ms (59.40%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
-SELECT name, population, area FROM World
-WHERE population >= 25000000 OR area >= 3000000;
+-- Write your PostgreSQL query statement below
+select name, population, area from World where population >= 25000000 or area >= 3000000
