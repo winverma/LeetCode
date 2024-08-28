daa9ede989d5f1c080a214ef91c6e3a5552fe18c
Win Verma <win.verma2021@vitstudent.ac.in>
2024-08-29 00:00:57 +0530

    Time: 329 ms (29.70%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
-select name from customer
-where referee_id != 2 or referee_id is null;
+-- Write your PostgreSQL query statement below
+select name from Customer where referee_id is null or referee_id != 2 
