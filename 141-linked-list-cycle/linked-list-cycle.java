b4ee9e6735e541da5ac0c1a55a0ca8d79599c654
Win Verma <win.verma2021@vitstudent.ac.in>
2024-09-02 00:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (50.15%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public boolean hasCycle(ListNode head) 
+    {
+        if(head == null) return false;
+
+        ListNode ptr1 = head;
+        ListNode ptr2 = head;
+
+        while(ptr2 != null && ptr2.next != null)
+        {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next.next;
+
+            if(ptr1 == ptr2) return true;
+        }
+
+        return false;
+    }
+}
