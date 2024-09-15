09b1f90435ca674102dc86062868e96f58102ab0
Win Verma <win.verma2021@vitstudent.ac.in>
2024-09-16 02:02:50 +0530

    Time: 26 ms (25.58%) | Memory: 40.5 MB (18.50%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.java
@@ -0,0 +1,29 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl 
+{
+    public int firstBadVersion(int n) 
+    {
+        int high = n;
+        int low = 0;
+        int answer = 0;
+
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+
+            if(isBadVersion(mid) == true)
+            {
+                answer = mid;
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        
+        return answer;
+    }
+}
