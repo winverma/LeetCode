c01af3f465fc656ce6a03ab7d91eb574f7735e29
Win Verma <win.verma2021@vitstudent.ac.in>
2024-09-24 02:29:35 +0530

    Time: 1 ms (98.66%) | Memory: 41.3 MB (13.17%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -2,6 +2,23 @@ class Solution
 {
     public int arrangeCoins(int n) 
     {
+        /*
+
+        1 + 2 + 3 + ... + x = n
+        (1 + x) * x / 2 = n
+
+        x^2 + x = 2n
+
+        x^2 + x + 1/4 = 2n + 1/4
+
+        (x + 1/2)^2 = 2n + 1/4
+
+        x + 0.5 = sqrt(2n + 0.25)
+
+        x = sqrt(2n + 0.25) - 0.5
+
+        */
+        
         long low  = 0;
         long high = n;
 
