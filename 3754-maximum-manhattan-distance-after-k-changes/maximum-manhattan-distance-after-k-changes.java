a50ca0b849bd9e72a927ea21955c117261507100
Win Verma <win.verma2021@vitstudent.ac.in>
2025-11-07 06:18:31 +0530

    Time: 58 ms (39.22%) | Memory: 47.9 MB (6.36%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.java
@@ -1,31 +1,22 @@
 class Solution {
     public int maxDistance(String s, int k) {
-        int north = 0;
-        int south = 0;
-        int east = 0;
-        int west = 0;
-
-        int maxDist = 0;
-
+        int ans = 0;
+        int north = 0, south = 0, east = 0, west = 0;
+        
         for (int i = 0; i < s.length(); i++) {
-            char dir = s.charAt(i);
-            if (dir == 'N') {
-                north++;
-            } else if (dir == 'S') {
-                south++;
-            } else if (dir == 'E') {
-                east++;
-            } else {
-                west++;
-            }
-
-            int base = Math.abs(north - south) + Math.abs(east - west);
-            int extra = Math.min(2 * k, i + 1 - base);
-            int total = base + extra;
-
-            maxDist = Math.max(maxDist, total);
+            char c = s.charAt(i);
+            if (c == 'N') north++;
+            else if (c == 'S') south++;
+            else if (c == 'E') east++;
+            else if (c == 'W') west++;
+            
+            int x = Math.abs(north - south);
+            int y = Math.abs(east - west);
+            int MD = x + y;      //MD =Manhattan Distance
+            int dis = MD + Math.min(2 * k, i + 1 - MD); // Ensure changes are at most k
+            ans = Math.max(ans, dis);
         }
-
-        return maxDist;
+        
+        return ans;
     }
 }
